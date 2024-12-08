eafa0240ea8b0ddc1f2c39df50d4ffaedb3b96cc
Przemysław Graczyk <graczyprzemysaw@meta.com>
2024-12-08 11:51:39 +0100

    Thrid Day - Update

--- advent_calendar/day_03.py
+++ advent_calendar/day_03.py
@@ -1,4 +1,4 @@
-import regex as re
+import re
 
 def load(path):
     with open(path, 'r') as file:
@@ -6,8 +6,18 @@ def load(path):
     return data
 
 def find_all_muls(data):
-    matches = re.findall("mul\(\d{1,5}\,\d{1,5}\)", data, flags=re.DOTALL)
+    matches = re.findall("mul\\(\\d{1,5}\\,\\d{1,5}\\)", data, flags=re.DOTALL)
     return matches
+
+def find_muls_with_do(data):
+    matches = re.findall("(do\(\))|(don't\(\))|(mul\(\d{1,5}\,\d{1,5}\))", data)
+    return matches
+
+def clean_do_array(matches):
+    return [" ".join(filter(None, t)) for t in matches]
+
+def replace():
+    pass
 
 def multiply(mul_string):
     mul_tuple = mul_string.replace('mul','').replace('(', '').replace(')', '').split(',')
@@ -19,5 +29,8 @@ def multiply(mul_string):
 if __name__ == '__main__':
     data = load('data/day_03.txt')
     matches = find_all_muls(data)
-    sum = sum([multiply(match) for match in matches])
-    print(sum)
+    sum_all = sum([multiply(match) for match in matches])
+    print(sum_all)
+    matches_do = clean_do_array(find_muls_with_do(data))
+    sum_do = ([match for match in matches_do])
+    print(sum_do)
